f6f2dd07a7f60bc2b6f6376a16f1ecee3accdd63
Julian Schnidder <julianschnidder@users.noreply.github.com>
2017-01-29 12:39:19 +0100

    Add files via upload

---
+++ patternsearch.m
@@ -0,0 +1,24 @@
+function [x,fval,exitflag,output] = patternsearch(f,x0,A,b,Aeq,beq,l,u,nonlcon,options)
+%x = patternsearch(f,x0)
+%x = patternsearch(f,x0,A,b)
+%x = patternsearch(f,x0,A,b,Aeq,beq)
+%x = patternsearch(f,x0,A,b,Aeq,beq,l,u)
+%x = patternsearch(f,x0,A,b,Aeq,beq,l,u,nonlcon)
+%x = patternsearch(f,x0,A,b,Aeq,beq,l,u,nonlcon,options)
+%x = patternsearch(problem)
+%[x,val] = patternsearch()
+%[x,val,exitstat,output] = patternsearch()
+
+switch nargin
+  case 0:
+    print("input expected, exiting");
+    return;
+    break;
+  case 1:
+    problem=f;
+    f=problem.f;
+    x0=problem.x0;
+    break;
+  case 2:
+    break;
+end    
